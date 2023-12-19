6eb66e05de6108472520cdd996904a4b731204a3
AlexPinkxXx <xandapin@gmail.com>
2023-12-18 16:50:13 -0800

    Updated Test #1

--- Beepboop/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ Beepboop/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -11,14 +11,44 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+private final CANSparkMax m_blMotor = new CANSparkMax(4, MotorType.kBrushless);
+MotorControllerGroup m_left = new MotorControllerGroup(m_flMotor, m_blMotor);
+private final CANSparkMax m_frMotor = new CANSparkMax(1, MotorType.kBrushless);
+private final CANSparkMax m_brMotor = new CANSparkMax(2, MotorType.kBrushless);
+MotorControllerGroup m_right = new MotorControllerGroup(m_frMotor, m_brMotor);
 
-public class Drivetrain extends SubsystemBase 
-{
-  private final CANSparkMax m_flMotor = new CANSparkMax(3, MotorType.kBrushless);
-  private final CANSparkMax m_blMotor = new CANSparkMax(4, MotorType.kBrushless);
-  MotorControllerGroup m_left = new MotorController Group(m_fl, m_blMotor);
-  private final CANSparkMax m_frMotor = new CANSparkMax(1, MotorType.kBrushless);
-  private final CANSparkMax m_brMotor = new CANSparkMax(2, MotorType.kBrushless);
+private final RelativeEncoder m_leftEncoder = m_flMotor.getEncoder();
+private final RelativeEncoder m_rightEncoder = m_frMotor.getEncoder();
+private final RelativeEncoder m_leftBackEncoder = m_blMotor.getEncoder();
+private final RelativeEncoder m_rightBackEncoder = m_brMotor.getEncoder();
 
-  
+// Set up the differential drive controller
+private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_left, m_right);
+
+private double balancePosition;
+
+public Drivetrain() {
+  m_flMotor.setIdleMode(IdleMode.kBrake);
+  m_blMotor.setIdleMode(IdleMode.kBrake);
+  m_frMotor.setIdleMode(IdleMode.kBrake);
+  m_brMotor.setIdleMode(IdleMode.kBrake);
+   // We need to invert one side of the drivetrain so that positive voltages result in both sides moving forward. Depending on how your robot's gearbox is constructed, you might have to invert the left side instead.
+  rightControllerGroup.setInverted(true);
+  leftControllerGroup.setInverted(false);
+  m_brMotor.setInverted(true);
+  m_frMotor.setInverted(true);
+  m_leftEncoder.setPositionConversionFactor(Constants.K_WHEEL_DIAMETER_INCH);
+  m_rightEncoder.setPositionConversionFactor(Constants.K_WHEEL_DIAMETER_INCH);
+  m_leftBackEncoder.setPositionConversionFactor(Constants.K_WHEEL_DIAMETER_INCH);
+  m_rightBackEncoder.setPositionConversionFactor(Constants.K_WHEEL_DIAMETER_INCH);
+  public void arcadeDrive(double fwd, double rot) {
+    differentialDrive.arcadeDrive(fwd, rot);
+  }
+  resetEncoders();
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
